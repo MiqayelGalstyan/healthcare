830ffcff3df9f96ed14b22183b5cab9fb2360c05
MiqayelGalstyan <miqayelgalstyan25@gmail.com>
2026-03-06 01:28:18 +0400

    add tailwind, add shadcnui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-white font-sans">
+      <p className="font-bold text-black text-2xl text-center">Home</p>
+      <Button>Click</Button>
+    </div>
+  );
+}
