6308db85a4614a701ed38fd42de21f2899afaee0
MiqayelGalstyan <miqayelgalstyan25@gmail.com>
2026-03-13 14:11:44 +0400

    fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
 import type { NextConfig } from "next";
 
-const supabaseUrl = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL!);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL)
+  : null;
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: supabaseUrl.hostname,
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
+    remotePatterns: supabaseUrl
+      ? [
+          {
+            protocol: "https",
+            hostname: supabaseUrl.hostname,
+            pathname: "/storage/v1/object/public/**",
+          },
+        ]
+      : [],
   },
 };
 
